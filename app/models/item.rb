5eea667dc47c3af39cfa95e0ea0636ef7cd0a229
supplepom <supplepom@me.com>
2020-12-29 01:55:55 +0900

    商品出品機能の実装

---
+++ app/models/item.rb
@@ -0,0 +1,31 @@
+class Item < ApplicationRecord
+
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :address
+  belongs_to :preparation_day
+
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :postage_id, numericality: { other_than: 1 } 
+  validates :address_id, numericality: { other_than: 1 } 
+  validates :preparation_day_id, numericality: { other_than: 1 } 
+  validates :price, presence: true
+
+  with_options presence: true, format: { with: /[0-9]/, message: 'is invalid. Input half-width number.' } do
+    validates :price
+  end
+
+  validates :price,numericality: { greater_than_or_equal_to: 300, less_than: 9999999, message: 'Out of setting range' }
+  
+  validates :image, presence: true
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の登録' do
+    context "商品が登録できる場合" do
+      it "imageとtitle,description,category_id,condition_id,postage_id,address_id,preparation_day_id,priceがあれば投稿できる" do
+        expect(@item).to be_valid
+      end
+    end
+    context "商品が登録できない場合" do
+      it "imageが空では投稿できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end     
+      it "titleが空では投稿できない" do
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Title can't be blank"
+      end  
+      it "descriptionが空では投稿できない" do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Description can't be blank"
+      end     
+      it "category_idが空では投稿できない" do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category must be other than 1"
+      end  
+      it "condition_idが空では投稿できない" do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition must be other than 1"
+      end       
+      it "postage_idが空では投稿できない" do
+        @item.postage_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Postage must be other than 1"
+      end 
+      it "preparation_day_idが空では投稿できない" do
+        @item.preparation_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Preparation day must be other than 1"
+      end 
+      it "priceが空では投稿できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end   
+      it 'priceが半角以外では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is invalid. Input half-width number."
+      end
+      it 'priceが¥300~¥9,999,999の間以外では登録できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price Out of setting range"
+      end
+    end
+  end
+end
