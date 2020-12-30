18e56d55b60846311d71b55559b24ccf6077b9b5
supplepom <supplepom@me.com>
2020-12-30 19:57:22 +0900

    修正コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,11 +37,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if item = Item.find(params[:id])
-    item.destroy
+    item = Item.find(params[:id])
+    if item.destroy
     redirect_to root_path
     else
-    redirect_to root_path
+    render :show
     end
   end
 
